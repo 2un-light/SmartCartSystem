4ea5abe69fa4c4d59739c6227c5019b63c3151d6
2un-light <sts0719@naver.com>
2021-04-12 03:10:52 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database  
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id' : 1,
-          'image' : 'https://placeimg.com/64/64/1',
-          'name' : '홍길순',
-          'birthday' : '961222',
-          'gender' : '여자',
-          'job' : '대학생'
-        },
-        {
-          'id' : 2,
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : '차은영',
-          'birthday' : '000902',
-          'gender' : '여자',
-          'job' : '대학생'
-        },
-        {
-          'id' : 3,
-          'image' : 'https://placeimg.com/64/64/3',
-          'name' : '홍길동',
-          'birthday' : '961222',
-          'gender' : '남자',
-          'job' : '대학생'
-        }
-        ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
