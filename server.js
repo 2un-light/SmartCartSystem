beaeacc6f5acc68ac13cabb4d6f651b6d080e61c
2un-light <sts0719@naver.com>
2021-04-14 00:44:04 +0900

    Implement CustomerAdd&CustomerList Refresh Module

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 app.get('/api/customers', (req, res) => {
     connection.query(
       "SELECT * FROM CUSTOMER",
@@ -28,5 +31,27 @@ app.get('/api/customers', (req, res) => {
       }
     );
 });
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'),(req,res)=>{
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let image = 'http://localhost:5000/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  
+
+
+  let params = [image, name, birthday, gender, job];
+
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+    );
+
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
